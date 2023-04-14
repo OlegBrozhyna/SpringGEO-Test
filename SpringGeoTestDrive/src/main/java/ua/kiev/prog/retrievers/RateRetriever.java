069196822bc5cc2cf9df79c58211608094eb4107
OlegBrozhyna <122048830+OlegBrozhyna@users.noreply.github.com>
2023-04-14 14:40:27 +0300

    commit

--- SpringGeoTestDrive/src/main/java/ua/kiev/prog/retrievers/RateRetriever.java
+++ SpringGeoTestDrive/src/main/java/ua/kiev/prog/retrievers/RateRetriever.java
@@ -9,26 +9,31 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ua.kiev.prog.json.Rate;
 
-@Component
+@Service
 public class RateRetriever {
 
-    private static final String key = "39mXC3PG1jwdjF20Wcu26OrDZn9og7CS";
+    private static final String KEY = "39mXC3PG1jwdjF20Wcu26OrDZn9og7CS";
     private static final String URL = "https://api.apilayer.com/fixer/latest?symbols=UAH&base=EUR";
 
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Cacheable(value = "rates", key = "'rate'")
     public Rate getRate() {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("apikey", key);
-        HttpEntity<String> entity = new HttpEntity<String>("testDrive", headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Rate> response = restTemplate.exchange(
-                URL,
-                HttpMethod.GET,
-                entity,
-                Rate.class
-        );
+        headers.set("apikey", KEY);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<Rate> response = new RestTemplate().exchange(URL, HttpMethod.GET, entity, Rate.class);
         return response.getBody();
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void updateRate() {
+        cacheManager.getCache("rates").evict("rate");
+        getRate();
     }
 }
